947ecf6438c1855733b1d12469ad80ac163d182c
Ewan Harris <ewan.harris@okta.com>
2024-04-18 17:37:57 +0100

    feat: support tranlasting existing yaml tests into gherkin syntax

--- internal/storetest/storedata.go
+++ internal/storetest/storedata.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"path"
 
+	"github.com/cucumber/godog"
 	"github.com/openfga/go-sdk/client"
 
 	"github.com/openfga/cli/internal/authorizationmodel"
@@ -35,11 +36,68 @@ type ModelTestCheck struct {
 	Assertions map[string]bool         `json:"assertions" yaml:"assertions"`
 }
 
+func (m *ModelTestCheck) toScenario() string {
+	scenarios := ""
+
+	for relation, value := range m.Assertions {
+		scenario := fmt.Sprintf("\n\tScenario: check %s %s %s\n", m.User, m.Object, relation)
+
+		scenario += fmt.Sprintf("\t\tWhen %s accesses %s\n", m.User, m.Object)
+		if m.Context != nil {
+			scenario += "\t\tWhen context is\n"
+
+			for key, value := range *m.Context {
+				scenario += fmt.Sprintf("\t\t\t| %s | %s |\n", key, value)
+			}
+		}
+
+		if value {
+			scenario += fmt.Sprintf("\t\tThen %s has the %s permission\n", m.User, relation)
+		} else {
+			scenario += fmt.Sprintf("\t\tThen %s does not have the %s permission\n", m.User, relation)
+		}
+
+		scenarios += scenario + "\n"
+	}
+
+	return scenarios
+}
+
 type ModelTestListObjects struct {
 	User       string                  `json:"user"       yaml:"user"`
 	Type       string                  `json:"type"       yaml:"type"`
 	Context    *map[string]interface{} `json:"context"    yaml:"context"`
 	Assertions map[string][]string     `json:"assertions" yaml:"assertions"`
+}
+
+func (m *ModelTestListObjects) toScenario() string {
+	scenarios := ""
+
+	for relation, objects := range m.Assertions {
+		scenario := fmt.Sprintf("Scenario: list objects %s %s %s\n", m.User, m.Type, objects)
+
+		if m.Context != nil {
+			scenario += "\t\tWhen context is\n"
+			for key, value := range *m.Context {
+				scenario += fmt.Sprintf("\t\t\t| %s | %s |\n", key, value)
+			}
+		}
+
+		scenario += fmt.Sprintf("\t\tWhen %s searches for %s\n", m.User, m.Type)
+
+		if len(objects) > 0 {
+			scenario += fmt.Sprintf("\t\tThen %s has the %s permission for\n", m.User, relation)
+			for _, object := range objects {
+				scenario += fmt.Sprintf("\t\t\t| %s |\n", object)
+			}
+		} else {
+			scenario += fmt.Sprintf("\t\tThen %s has no %s permission\n", m.User, relation)
+		}
+
+		scenarios += scenario + "\n"
+	}
+
+	return scenarios
 }
 
 type ModelTest struct {
@@ -49,6 +107,47 @@ type ModelTest struct {
 	TupleFile   string                            `json:"tuple_file"   yaml:"tuple_file"` //nolint:tagliatelle
 	Check       []ModelTestCheck                  `json:"check"        yaml:"check"`
 	ListObjects []ModelTestListObjects            `json:"list_objects" yaml:"list_objects"` //nolint:tagliatelle
+}
+
+func (m *ModelTest) toFeature(globalTuples string) godog.Feature {
+	featureString := fmt.Sprintf("Feature: %s\n", m.Name)
+
+	localTuples := ""
+	if len(m.Tuples) > 0 {
+		localTuples = tuplesToGherkin(m.Tuples)
+	}
+
+	if globalTuples != "" || localTuples != "" {
+		featureString += "\tBackground:\n" + globalTuples + localTuples
+	}
+
+	featureString += m.toCheckScenarios()
+	featureString += m.toListObjectsScenarios()
+
+	return godog.Feature{
+		Name:     m.Name,
+		Contents: []byte(featureString),
+	}
+}
+
+func (m *ModelTest) toCheckScenarios() string {
+	scenarios := ""
+
+	for _, check := range m.Check {
+		scenarios += "\n\t" + check.toScenario()
+	}
+
+	return scenarios
+}
+
+func (m *ModelTest) toListObjectsScenarios() string {
+	scenarios := ""
+
+	for _, check := range m.ListObjects {
+		scenarios += "\n\t" + check.toScenario()
+	}
+
+	return scenarios
 }
 
 type StoreData struct {
@@ -122,4 +221,36 @@ func (storeData *StoreData) LoadTuples(basePath string) error {
 	}
 
 	return nil
+}
+
+func (storeData *StoreData) ToFeatures() []godog.Feature {
+	features := []godog.Feature{}
+	globalTuples := ""
+
+	if len(storeData.Tuples) > 0 {
+		globalTuples += tuplesToGherkin(storeData.Tuples)
+	}
+
+	for _, test := range storeData.Tests {
+		features = append(features, test.toFeature(globalTuples))
+	}
+
+	return features
+}
+
+func tuplesToGherkin(tuples []client.ClientContextualTupleKey) string {
+	givenString := ""
+	for _, tuple := range tuples {
+		givenString += fmt.Sprintf("\t\tGiven %s is a %s of %s", tuple.User, tuple.Relation, tuple.Object)
+		if tuple.Condition != nil {
+			givenString += fmt.Sprintf(" with %s being\n", tuple.Condition.Name)
+			for key, value := range *tuple.Condition.Context {
+				givenString += fmt.Sprintf("\t\t\t| %s | %s |\n", key, value)
+			}
+		}
+
+		givenString += "\n"
+	}
+
+	return givenString
 }
