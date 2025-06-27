014ad0e45ecd0d1d0b48f047f4bd3d21ba8a2f14
Andrés Aguiar <andres.aguiar@gmail.com>
2025-06-27 11:02:07 -0300

    refactor tuple write logging

--- internal/utils/context.go
+++ internal/utils/context.go
@@ -1,6 +1,9 @@
 package utils
 
-import "context"
+import (
+	"context"
+	"os"
+)
 
 type contextKey struct {
 	name string
@@ -16,4 +19,31 @@ func GetDebugContextValue(ctx context.Context) bool {
 	debug, _ := ctx.Value(keyDebug).(bool)
 
 	return debug
+}
+
+var (
+	keySuccessLog = contextKey{"successLog"}
+	keyFailureLog = contextKey{"failureLog"}
+)
+
+// WithSuccessLog attaches a success log file to the context.
+func WithSuccessLog(ctx context.Context, file *os.File) context.Context {
+	return context.WithValue(ctx, keySuccessLog, file)
+}
+
+// WithFailureLog attaches a failure log file to the context.
+func WithFailureLog(ctx context.Context, file *os.File) context.Context {
+	return context.WithValue(ctx, keyFailureLog, file)
+}
+
+// GetSuccessLog retrieves the success log file from the context.
+func GetSuccessLog(ctx context.Context) *os.File {
+	file, _ := ctx.Value(keySuccessLog).(*os.File)
+	return file
+}
+
+// GetFailureLog retrieves the failure log file from the context.
+func GetFailureLog(ctx context.Context) *os.File {
+	file, _ := ctx.Value(keyFailureLog).(*os.File)
+	return file
 }
